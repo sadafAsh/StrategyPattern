999e703757148e86e84f7f665aece11a9c454442
test@gmail.com <test@gmail.com>
2022-02-17 10:58:52 +0000

    init commit

---
+++ src/main/java/com/soj/pattern/strategy/Main.java
@@ -0,0 +1,13 @@
+package com.soj.pattern.strategy;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class Main {
+    static  final Logger logger= LogManager.getLogger(Main.class.getName());
+    public static void main(String[] args){
+
+
+    }
+}
